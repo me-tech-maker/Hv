877dfcafbc9da56e54ac57da6ae115666429bf6f
me-tech-maker <nixiechan85@gmail.com>
2025-09-28 13:53:15 -0300

    Update yt-stalk.js

--- plugins/yt-stalk.js
+++ plugins/yt-stalk.js
@@ -6,7 +6,7 @@ cmd({
   alias: ["ytinfo"],
   desc: "Get details about a YouTube channel.",
   react: "🔍",
-  category: "SCH",
+  category: "search",
   filename: __filename
 }, async (conn, m, store, { from, quoted, q, reply }) => {
   try {
